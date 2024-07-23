1597808384eb0cff85ecbfe54a82548b406a940f
Jason Worthy <kjasonworthy@gmail.com>
2024-07-23 19:30:26 -0400

    Count to Eleven

---
+++ src/main/java/org/example/CountToEleven.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class CountToEleven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number less than 11:");
+        int number = scanner.nextInt();
+
+        if (number < 11) {
+            for (int i = number; i <= 11; i++) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("The number must be less than 11.");
+        }
+    }
+}
